74407738e138c5fd8e15838cc2606289a0b39963
Simon Sun <sunmingyu.97@gmail.com>
2021-05-17 21:53:07 -0500

    Create test2.py
    first push

---
+++ test2.py
@@ -0,0 +1,18 @@
+class Base(object):
+    """docstring for Base."""
+
+    def __init__(self, id=0):
+        super(Base, self).__init__()
+        self.id = id
+
+a = Base()
+b = Base()
+print(a)
+print(b)
+print(type(a))
+print(2+3)
+print(3+4)
+print(3 * 5)
+print(2 ** 3)
+###########
+# Time of coding: 5/17/2021 9:51 PM
